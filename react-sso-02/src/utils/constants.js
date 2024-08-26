30e543099478741843c1b028cf4825329db54bad
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-08-27 01:24:15 +0700

    change url

--- react-sso-02/src/utils/constants.js
+++ react-sso-02/src/utils/constants.js
@@ -1,3 +1,3 @@
 // Author: TrungQuanDev | https://youtube.com/@trungquandev
-export const RENDER_API_ENPOINT = 'https://node-server-auth0-sso-ehnt.onrender.com'
+export const RENDER_API_ENPOINT = 'https://auth0-sso-react-node.onrender.com'
 export const LOCAL_API_ENPOINT = 'http://localhost:8017'
